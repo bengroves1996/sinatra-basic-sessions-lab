91b6175d3626f12ee287245df467d58ea00f4790
bengroves <bengroves15@gmail.com>
2020-12-02 18:21:39 -0500

    commit tree 8e07906748c52768db0bdc5003fd4b7dad30d24c

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
+configure do
+    enable :sessions
+    set :session_secret, "doge"
+end 
 
 class App < Sinatra::Base
+
+
+    get '/' do
+        erb :index
+    end 
+
+    post '/checkout' do 
+        
+        @input = params[:item]
+        session['item'] = "#{@input}"
+        @session = session
+        erb :index
+    end 
 end
